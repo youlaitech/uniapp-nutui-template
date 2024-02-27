9a750e972e718eb8b8b3ff41325590226d134668
hxr <1490493387@qq.com>
2024-02-28 00:21:50 +0800

    feat: 整合 unocss 和微信小程序

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,72 +1,66 @@
-<script setup lang="ts">
-	import BasicButton from '@/components/BasicButton/index.vue';
-	import AppProvider from '@/components/AppProvider/index.vue';
-	import { CURRENT_PLATFORM, PLATFORMS } from '@/enums/platformEnum';
-	import { judgePlatform } from '@/utils/platform';
-	import Iconify from '@/components/Iconify/index.vue';
-	import { getEnvValue } from '@/utils/env';
-
-	const appTitle = getEnvValue<string>('VITE_APP_TITLE');
-
-	const title = ref(appTitle);
-
-	const platform = CURRENT_PLATFORM;
-
-	const isVue3 = judgePlatform(PLATFORMS.VUE3);
-
-	const router = useRouter();
-	const handleGetStarted = () => {
-		router.pushTab({ path: '/pages/demo/index' });
-	};
-</script>
 <template>
-	<AppProvider>
-		<view class="content">
-			<nut-button type="primary">
-				主要按钮
-			</nut-button>
-			<image class="logo" src="/static/svg/LOGO.svg" />
-			<view class="text-area">
-				<text class="">{{ title }}</text>
-			</view>
-			<view class="text-area">
-				<text class="">是否是Vue3: {{ isVue3 }}</text>
-			</view>
-			<view class="text-area">
-				<text class="_u_text-red">当前平台: {{ platform }}</text>
-			</view>
-			<BasicButton @click="handleGetStarted">Get Started → </BasicButton>
-			<view class="_u_text-red">uno css</view>
-			<Iconify icon="i-ph-anchor-simple-thin" size="65" />
-			<Iconify icon="i-system-uicons-book-text" />
-			<Iconify icon="i-system-uicons-battery-full" size="65" />
-			<Iconify icon="i-system-uicons-box-add" :size="65" />
-			<Iconify icon="i-system-uicons-bell-snooze" color="red" :size="65" />
-		</view>
-	</AppProvider>
+    <AppProvider>
+        <view class="content">
+            <nut-button type="primary"> 主要按钮 </nut-button>
+            <image class="logo" src="/static/svg/LOGO.svg" />
+            <view class="text-area">
+                <text class="">{{ title }}</text>
+            </view>
+            <view class="text-area">
+                <text class="">是否是Vue3: {{ isVue3 }}</text>
+            </view>
+            <view class="text-area">
+                <text class="text-red">当前平台: {{ platform }}</text>
+            </view>
+            <BasicButton @click="handleGetStarted">Get Started → </BasicButton>
+        </view>
+    </AppProvider>
 </template>
+
+<script setup lang="ts">
+import BasicButton from '@/components/BasicButton/index.vue';
+import AppProvider from '@/components/AppProvider/index.vue';
+import { CURRENT_PLATFORM, PLATFORMS } from '@/enums/platformEnum';
+import { judgePlatform } from '@/utils/platform';
+import Iconify from '@/components/Iconify/index.vue';
+import { getEnvValue } from '@/utils/env';
+
+const appTitle = getEnvValue<string>('VITE_APP_TITLE');
+
+const title = ref(appTitle);
+
+const platform = CURRENT_PLATFORM;
+
+const isVue3 = judgePlatform(PLATFORMS.VUE3);
+
+const router = useRouter();
+const handleGetStarted = () => {
+    router.pushTab({ path: '/pages/demo/index' });
+};
+</script>
+
 <style lang="scss">
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+.content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 280rpx auto 50rpx;
-	}
+.logo {
+    height: 200rpx;
+    width: 200rpx;
+    margin: 280rpx auto 50rpx;
+}
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-		margin-bottom: 60rpx;
-	}
+.text-area {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 60rpx;
+}
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+.title {
+    font-size: 36rpx;
+    color: #8f8f94;
+}
 </style>
--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,3 +1,29 @@
+<template>
+    <view class="container">
+        <!-- #ifdef MP -->
+        <nut-button block type="primary">小程序登录授权</nut-button>
+        <view class="text-xs mt-2"> 温馨提示:未注册有来小店的用户,初次登录时将完成注册 </view>
+        <!-- #endif -->
+
+        <!-- #ifndef MP -->
+        <view class="title">登录</view>
+        <view class="form-wrap">
+            <form class="form" @submit="submit">
+                <label class="form-item">
+                    <view class="form-label">邮箱:</view>
+                    <view class="form-element"><input name="email" :value="form.email" /></view>
+                </label>
+                <label class="form-item">
+                    <view class="form-label">密码:</view>
+                    <view class="form-element"><input type="password" name="password" :value="form.password" /></view>
+                </label>
+                <button form-type="submit" class="submit-btn" hover-class="none">登录</button>
+            </form>
+        </view>
+        <!-- #endif -->
+    </view>
+</template>
+
 <script setup lang="ts">
 import { useAuthStore } from '@/state/modules/auth';
 import { Toast } from '@/utils/uniapi/prompt';
@@ -41,25 +67,6 @@ const submit = (e: any) => {
     });
 };
 </script>
-
-<template>
-    <view class="container">
-        <view class="title">登录</view>
-        <view class="form-wrap">
-            <form class="form" @submit="submit">
-                <label class="form-item">
-                    <view class="form-label">邮箱:</view>
-                    <view class="form-element"><input name="email" :value="form.email" /></view>
-                </label>
-                <label class="form-item">
-                    <view class="form-label">密码:</view>
-                    <view class="form-element"><input type="password" name="password" :value="form.password" /></view>
-                </label>
-                <button form-type="submit" class="submit-btn" hover-class="none">登录</button>
-            </form>
-        </view>
-    </view>
-</template>
 
 <style lang="scss" scoped>
 .container {
